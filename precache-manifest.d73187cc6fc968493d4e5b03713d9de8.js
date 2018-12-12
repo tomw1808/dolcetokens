ff28768a3ffdeadacc2776abe49146f9fb2a28ef
Thomas Wiesner <tomw1808@gmail.com>
2018-12-12 15:53:55 +0100

    updated version

---
+++ precache-manifest.d73187cc6fc968493d4e5b03713d9de8.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9f6270e92fe92e04639dafdc79171f8",
+    "url": "/dolcetokens/static/media/flour.e9f6270e.jpg"
+  },
+  {
+    "revision": "7e8bc72d66ba5a3adac5",
+    "url": "/dolcetokens/static/css/main.3d79ad7a.chunk.css"
+  },
+  {
+    "revision": "5022186b7d7d3e8672377253b246150a",
+    "url": "/dolcetokens/static/media/tomato.5022186b.jpg"
+  },
+  {
+    "revision": "98058e94965f4383d577",
+    "url": "/dolcetokens/static/js/1.98058e94.chunk.js"
+  },
+  {
+    "revision": "05fcc4ba99bc5c4713df",
+    "url": "/dolcetokens/static/js/runtime~main.05fcc4ba.js"
+  },
+  {
+    "revision": "279a20a4c23055020724aa1a65c18cdd",
+    "url": "/dolcetokens/static/media/bread.279a20a4.jpg"
+  },
+  {
+    "revision": "bb5883926aacd01cf144541ee3d40b11",
+    "url": "/dolcetokens/static/media/bun.bb588392.jpg"
+  },
+  {
+    "revision": "16812e2d76d057e8a47c5cc806fe16c5",
+    "url": "/dolcetokens/static/media/cherry.16812e2d.jpg"
+  },
+  {
+    "revision": "0cda2a237e6f6df1c5af98bb852a322c",
+    "url": "/dolcetokens/static/media/cookies.0cda2a23.jpg"
+  },
+  {
+    "revision": "83bd8476d1a4fb868bdeed6561effff6",
+    "url": "/dolcetokens/static/media/cupcakes.83bd8476.jpg"
+  },
+  {
+    "revision": "bfc567026f7be84713b66d3f6bc41b6a",
+    "url": "/dolcetokens/static/media/donut.bfc56702.jpg"
+  },
+  {
+    "revision": "ea1ec4e31c84b55c3c077b7a51a0914d",
+    "url": "/dolcetokens/static/media/dough.ea1ec4e3.jpg"
+  },
+  {
+    "revision": "8af8621cfaafef31fa65ce3204ef53a0",
+    "url": "/dolcetokens/static/media/eggs.8af8621c.jpg"
+  },
+  {
+    "revision": "7e8bc72d66ba5a3adac5",
+    "url": "/dolcetokens/static/js/main.7e8bc72d.chunk.js"
+  },
+  {
+    "revision": "ce863ebfee7b4b7e8a2e54b84170a2b6",
+    "url": "/dolcetokens/static/media/hotdog.ce863ebf.jpg"
+  },
+  {
+    "revision": "0b00c6a57523fc34b5af18711832921d",
+    "url": "/dolcetokens/static/media/ketchup.0b00c6a5.jpg"
+  },
+  {
+    "revision": "23fba89b5696b26424c762a2975dd69a",
+    "url": "/dolcetokens/static/media/layercake.23fba89b.jpg"
+  },
+  {
+    "revision": "b07a8dec677bc4d4aee59b786343c3b6",
+    "url": "/dolcetokens/static/media/lettuce.b07a8dec.jpg"
+  },
+  {
+    "revision": "be9c91a3e9f61513ad88d0876df002c6",
+    "url": "/dolcetokens/static/media/meat.be9c91a3.jpg"
+  },
+  {
+    "revision": "4185e853992bec923f1e06b7451186d3",
+    "url": "/dolcetokens/static/media/milk.4185e853.jpg"
+  },
+  {
+    "revision": "db3dc86a2f499ba3b99c17311b1cce94",
+    "url": "/dolcetokens/static/media/mustard.db3dc86a.jpg"
+  },
+  {
+    "revision": "540da8353a7796824cc651833f85f920",
+    "url": "/dolcetokens/static/media/sausage.540da835.jpg"
+  },
+  {
+    "revision": "f9aabd1c5665efaa8d89cb63f602444e",
+    "url": "/dolcetokens/static/media/sprinkle.f9aabd1c.jpg"
+  },
+  {
+    "revision": "31700d0364c3a7b2e955f96033e73147",
+    "url": "/dolcetokens/static/media/sprinkledcupcakes.31700d03.jpg"
+  },
+  {
+    "revision": "c09add2387285929da65136f2e94c0ed",
+    "url": "/dolcetokens/static/media/sugar.c09add23.jpg"
+  },
+  {
+    "revision": "98058e94965f4383d577",
+    "url": "/dolcetokens/static/css/1.8cd04535.chunk.css"
+  },
+  {
+    "revision": "94f21f6b8ccd6c96a6e9ae315c379e19",
+    "url": "/dolcetokens/index.html"
+  }
+];
